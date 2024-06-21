eed4e126b1f47d84e059926b04d29cee1479bcb4
wadoyoka <122070165+wadoyoka@users.noreply.github.com>
2024-06-21 11:17:33 +0900

    testPromotion

--- app.js
+++ app.js
@@ -1,6 +1,31 @@
 const express = require("express");
+const expressWs = require('express-ws')
 const app = express();
+expressWs(app);
+
 const port = process.env.PORT || 3001;
+let connects = [];
+
+app.use(express.static('public'))
+
+app.ws('/ws', (ws, req) => {
+  connects.push(ws)
+
+  ws.on('message', (message) => {
+    console.log('Received:', message)
+
+    connects.forEach((socket) => {
+      if (socket.readyState === 1) {
+        // Check if the connection is open
+        socket.send(message)
+      }
+    })
+  })
+
+  ws.on('close', () => {
+    connects = connects.filter((conn) => conn !== ws)
+  })
+})
 
 app.get("/", (req, res) => res.type('html').send(html));
 
